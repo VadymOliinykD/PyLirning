886c60fed595bc823de9858c63c99b4bea008548
VadymOliinykD <vadim.oliynykd@gmail.com>
2024-12-29 15:02:54 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,21 @@
+# import random
+# number = 0
+# a = random.randrange(0,10)
+# while number < 3 :
+#     b = int(input("вгадайте число"))
+#     if a == b :
+#         print ("ви вгадали")
+#         break
+#     elif a < b : 
+#         print("задумане число менше")        
+#     elif a > b :
+#         print ("задумане число білше")
+#     if number == 2 :
+#         print("ви програли")
+#     number += 1
+    
+
+
+rock = 'google'
+for a in rock:
+    print (a)
